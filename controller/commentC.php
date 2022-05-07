2bd23ef300dd9ba20e6df2e24ab46686b6e5f6f4
ahmed turki <turki.ahmed@esprit.tn>
2022-05-07 13:50:04 +0100

    v3

--- controller/commentC.php
+++ controller/commentC.php
@@ -1,11 +1,11 @@
 <?php
-	include_once '../../config.php';
+	include '../../config1.php';
 	require_once '../../model/commentaire.php';
     $update=false;
 	class commentC {
 		function afficherComment(){
 			$sql="SELECT * FROM commentaireblog";
-			$db = config::getConnexion();
+			$db = config1::getConnexion();
 			try{
 				$liste = $db->query($sql);
 				return $liste;
@@ -19,7 +19,7 @@
 			$sql="INSERT INTO commentaireblog (contenuC, id_blog) VALUES ( :contenuC, :id_blog)"; 
             //INSERT INTO `commentaireblog` (`contenuC`, `id_blog`) VALUES ( 'test', 2);
 			
-			$db = config::getConnexion();
+			$db = config1::getConnexion();
 			try{
 				$query = $db->prepare($sql);
 				$query->execute([
@@ -34,10 +34,10 @@
 			}	
             //header("Location: blogDetails.php");		
 		}
-		
+
 		function supprimercommentC($idCommentaire){
 			$sql="DELETE FROM commentaireblog WHERE idCommentaire=:idCommentaire";
-			$db = config::getConnexion();
+			$db = config1::getConnexion();
 			$req=$db->prepare($sql);
 			$req->bindValue(':idCommentaire', $idCommentaire);
 			try{
@@ -47,29 +47,7 @@
 				die('Erreur:'. $e->getMessage());
 			}
 		}
-
-		function modifierblog($blog, $idCommentaire){
-			try {
-				$db = config::getConnexion();
-				$query = $db->prepare(
-					'UPDATE commentaireblog SET 
-						dateC= :dateC,
-						contenuC= :contenuC 
-					
-					WHERE idCommentaire= :idCommentaire'
-				);
-				$query->execute([
-					'idCommentaire' => $blog->getidCommentaire(),
-					'dateC' => $blog->getdateC(),
-					'contenuC' => $blog->getcontenuC()
-					
-					
-				]);
-				echo $query->rowCount() . " records UPDATED successfully <br>";
-			} catch (PDOException $e) {
-				$e->getMessage();
-			}
-		}
+		
 		
 		
 
